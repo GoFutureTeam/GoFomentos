5ec0465798be3cee3d5a75d20e110c146e512b97
Eduarda <dev.eduardalmeida@gmail.com>
2025-09-25 16:11:45 -0300

    Recronstrução da estrutura do projeto e mudança de versão do Angular CLI

---
+++ frontend/src/app/pages/editais/edital-search-page/edital-search-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edital-search-page',
+  imports: [],
+  templateUrl: './edital-search-page.component.html',
+  styleUrl: './edital-search-page.component.scss'
+})
+export class EditalSearchPageComponent {
+
+}
---
+++ frontend/src/app/shared/components/filter-dropdown/filter-dropdown.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-dropdown',
+  imports: [],
+  templateUrl: './filter-dropdown.component.html',
+  styleUrl: './filter-dropdown.component.scss'
+})
+export class FilterDropdownComponent {
+
+}
